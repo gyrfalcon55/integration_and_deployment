87c6e9ae24d32df80131a3bd6bc323a90d01b36e
sjunaid <sjunad@gmail.com>
2026-03-25 20:36:06 +0530

    normal

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,7 +4,4 @@ def test_add():
     assert add(2, 3) == 5
 
 def test_model_predict():
-    try:
-        assert model_predict(3) == 8
-    except:
-        print("Error test case failed")
+    assert model_predict(3) == 8
